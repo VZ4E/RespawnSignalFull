57246d724b17c37d58911c284796ef4050d8dfd1
VZ4E <ajayistaken@gmail.com>
2026-03-20 02:17:12 -0400

    Update youtubeScanner.js

--- src/services/youtubeScanner.js
+++ src/services/youtubeScanner.js
@@ -85,7 +85,7 @@ async function detectBrands(channel, videos) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'llama-3.1-sonar-large-128k-online',
+      model: 'sonar',
       messages: [
         {
           role: 'system',
